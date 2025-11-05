74a5e7396f8211f27566be431624a4cafa87308c
Adam Braccia <111795014+abraccia@users.noreply.github.com>
2025-11-05 12:45:29 -0500

    test

--- client.py
+++ client.py
@@ -1,10 +1,14 @@
+#Client VM: Kali 4001
+#Clients IP: 192.168.204.209
+#Port: 4444
+
 import socket
 
-HOST = input("Enter the Server's IP Address")
-PORT = input("Enter the port used by the server")
+HOST = input("Enter the Server's IP Address: ")
+#PORT = input("Enter the port used by the server: ")
 
 #HOST = "127.0.0.1"  # The server's hostname or IP address
-#PORT = 65432  # The port used by the server
+PORT = 65432  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
--- server.py
+++ server.py
@@ -1,10 +1,13 @@
+#Server VM: Kali 3999
+#Server IP: 192.168.193.113
+#Port: 4444
 import socket
 
-HOST = input("Enter your target IP Address")
-PORT = input("Enter your target Port")
+HOST = input("Enter your target IP Address: ")
+#PORT = input("Enter your target Port: ")
 
 #HOST = "127.0.0.1"
-#PORT = 65432
+PORT = 65432
 print("Trying to connect to client...")
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
